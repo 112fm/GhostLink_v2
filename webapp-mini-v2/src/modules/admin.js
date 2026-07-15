78ddc9b9ec8b9d063a77eaa4dd22beead7c3448f
112FM <7artmedia69@gmail.com>
2026-07-15 21:39:43 +0300

    fix: revert api port to 443 due to port 2053 block

--- webapp-mini-v2/src/modules/admin.js
+++ webapp-mini-v2/src/modules/admin.js
@@ -1,4 +1,4 @@
-import { apiFetch, getApiBase } from "../api/client.js?v=20260715-miniapp-release-21";
+import { apiFetch, getApiBase } from "../api/client.js?v=20260715-miniapp-release-22";
 
 function setStatus(node, text, isError = false) {
   if (!node) return;
--- webapp-mini-v2/src/modules/devices.js
+++ webapp-mini-v2/src/modules/devices.js
@@ -1,4 +1,4 @@
-import { apiFetch } from "../api/client.js?v=20260715-miniapp-release-21";
+import { apiFetch } from "../api/client.js?v=20260715-miniapp-release-22";
 
 function setStatus(node, text, isError = false) {
   if (!node) return;
