bf1970c1cfebb6904e838842e16eb200486b31c8
112FM <7artmedia69@gmail.com>
2026-07-15 22:27:46 +0300

    fix: revert to 443 permanently as 2053 is blocked

--- webapp-mini-v2/src/modules/admin.js
+++ webapp-mini-v2/src/modules/admin.js
@@ -1,4 +1,4 @@
-import { apiFetch, getApiBase } from "../api/client.js?v=20260715-miniapp-release-23";
+import { apiFetch, getApiBase } from "../api/client.js?v=20260715-miniapp-release-24";
 
 function setStatus(node, text, isError = false) {
   if (!node) return;
