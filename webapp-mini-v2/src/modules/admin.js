9cfd0ce94b755f659b442d2e39c5d46fadeb7a8c
112FM <7artmedia69@gmail.com>
2026-07-17 13:48:19 +0300

    fix: enforce panel.112prd.ru bypass over other agent's override

--- webapp-mini-v2/src/modules/admin.js
+++ webapp-mini-v2/src/modules/admin.js
@@ -1,4 +1,4 @@
-import { apiFetch, getApiBase } from "../api/client.js?v=20260715-miniapp-release-27";
+import { apiFetch, getApiBase } from "../api/client.js?v=20260715-miniapp-release-28";
 
 function setStatus(node, text, isError = false) {
   if (!node) return;
